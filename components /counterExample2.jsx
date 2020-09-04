b6959f577b4328dda9be8e8e12bd7a1853f5808a
Arsalon-amini <57127012+Arsalon-amini@users.noreply.github.com>
2020-09-05 00:33:44 +0800

    Components Commit
    JSX components for reading lists dynamically, adding classes to jsx for styling, etc.

---
+++ components /counterExample2.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0, //state -> obj includes any data .this component needs
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
+        <button className="btn btn-secondary btn-sm">increment</button>{" "}
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}> {tag}</li>
+          ))}{" "}
+          {/*//getting string, mapping to jsx expression, combile to react element, JS object*/}
+        </ul>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state; //picking count property of .this obj storing in const count(destructuring)
+    return count === 0 ? "Zero" : this.state.count;
+  }
+}
+
+export default Counter;
