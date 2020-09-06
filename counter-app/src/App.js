574f868f97eb71399e8439ba5eb60a39983f5dea
Arsalon-amini <57127012+Arsalon-amini@users.noreply.github.com>
2020-09-06 19:21:27 +0800

    Lifting Up State
    Moving State to App component to create a component tree in which children components can access App state data for synchronizing between one another

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -2,15 +2,55 @@ import React, { Component } from "react";
 import NavBar from "./components/navbar";
 import Counters from "./components/counters"; //custom component
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  //updating state
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId); //create a new array without item, have react save as new state
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c; //map method returns a new array (new State obj)
+    });
+    this.setState({ counters }); //update state with new State obj
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter }; //clone counter obj@ [index]
+    counters[index].value++; //update state in new counter obj VS. state in old counter obj (no-no in React)
+    this.setState({ counters }); //update state with new counters array (cloned / modified)
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters} //sent via props to child
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement} //data & methods controlled by parent*/
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -2,52 +2,21 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  //updating state
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId); //create a new array without item, have react save as new state
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c; //map method returns a new array (new State obj)
-    });
-    this.setState({ counters }); //update state with new State obj
-  };
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter }; //clone counter obj@ [index] 
-    counters[index].value++; //update state in new counter obj VS. state in old counter obj (no-no in React)
-    this.setState({ counters }); //update state with new counters array (cloned / modified) 
-  };
-
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-sm m-2"
         >
           {" "}
           Reset{" "}
         </button>
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter} //can access id and value from counter obj
           ></Counter>
         ))}
@@ -57,3 +26,6 @@ class Counters extends Component {
 }
 
 export default Counters;
+
+
+
