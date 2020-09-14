1ca07e9976ad579ef8cf1708db280156a1e66176
Arsalon-amini <57127012+Arsalon-amini@users.noreply.github.com>
2020-09-14 16:43:30 +0800

    React Routing
    react-router-dom
    
    History, Location, Match props
    
    Router, Link, Nested Routing, Redirects, Query String Parameters, Router Parameters, etc.

---
+++ router-app/src/components/posts.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import queryString from "query-string";
+
+const Posts = ({ match, location }) => {
+  const { sortBy } = queryString.parse(location.search); //returns obj w/ queryString extracted from <Router> location.search
+  console.log(sortBy);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      Year: {match.params.year}, Month: {match.params.month}
+    </div>
+  );
+};
+
+export default Posts;
