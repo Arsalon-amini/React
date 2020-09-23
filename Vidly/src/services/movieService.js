dc51037eaa1b6531a800868ba08873dfb433fafc
Arsalon-amini <57127012+Arsalon-amini@users.noreply.github.com>
2020-09-23 14:30:39 +0800

    Refactoring - updated componentDidMount ( ) , movieService ( )

--- Vidly/src/services/movieService.js
+++ Vidly/src/services/movieService.js
@@ -3,23 +3,27 @@ import { apiUrl } from "../config.json";
 
 const apiEndpoint = apiUrl + "/movies";
 
+function movieUrl(id){
+  return `${apiEndpoint}/${id}`; //dynamically render ${ whatsInside }
+}
+
 export function getMovies() {
   return http.get(apiEndpoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndpoint + "/" + movieId);
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie }; //clone state obj
     delete body._id; //removie _id property (Mongoose will auto create ID property)
-    return http.put(apiEndpoint + "/" + movie._id, body);
+    return http.put(mvoieUrl(movie._id), body);
   }
   return http.post(apiEndpoint, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + "/" + movieId); //axios call endpoint .delete
+  return http.delete(movieUrl(movieId)); //axios call endpoint .delete
 }
